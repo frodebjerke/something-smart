28fb4d1f3b6f19abcccc8d3fb5de66f0896fde25
Frode Bjerke <frode.bjerke@gmail.com>
2016-03-25 13:10:01 +0100

    Refactoring :turtle:

--- server/config/rethinkdb/index.js
+++ server/config/rethinkdb/index.js
@@ -1,6 +1,27 @@
 import r, { setupDb } from './connection';
 import indices from './indices';
 import tables, { ensureTables} from './tables';
+import co from 'co';
+import winston from 'winston';
+
+function migrations() {
+  co(setupDb())
+  .then(() => {
+    winston.info("db setup done")
+    return co(ensureTables(r))
+      .catch((err) => {
+        winston.error('Failed to run database setup', err);
+        throw new Error('Could not start server');
+      })
+  })
+  .then(() => {
+    return co(indices())
+      .catch((err) => {
+        winston.error('Failed to ensure indices');
+        throw new Error('Could not start server', err);
+      })
+  }).catch((err) => { console.log(err)})
+}
 
 export default r;
-export { indices, tables, setupDb, ensureTables };
+export { indices, tables, setupDb, ensureTables, migrations };
--- server/index.js
+++ server/index.js
@@ -1,28 +1,9 @@
 import winston from 'winston';
 import App from './app';
 import co from 'co';
-import r, {
-  setupDb,
-  ensureTables,
-  indices
-} from './config/rethinkdb';
+import { migrations } from './config/rethinkdb';
 
-co(setupDb())
-.then(() => {
-  winston.info("db setup done")
-  return co(ensureTables(r))
-    .catch((err) => {
-      winston.error('Failed to run database setup', err);
-      throw new Error('Could not start server');
-    })
-})
-.then(() => {
-  return co(indices())
-    .catch((err) => {
-      winston.error('Failed to ensure indices');
-      throw new Error('Could not start server', err);
-    })
-}).catch((err) => { console.log(err)})
+migrations();
 
 const app = App();
 const port = process.env.PORT || 3000;
